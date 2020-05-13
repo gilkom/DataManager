30e897734c63794a12fc36a6215da83c7e8b0cb8
gilkom <gilek06@op.pl>
2020-05-13 20:20:33 +0200

    adding handler to controller

--- src/main/java/gilko/marcin/datamanager/controller/AppController.java
+++ src/main/java/gilko/marcin/datamanager/controller/AppController.java
@@ -1,12 +1,23 @@
 package gilko.marcin.datamanager.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import gilko.marcin.datamanager.model.Product;
 import gilko.marcin.datamanager.service.ProductService;
 
 @Controller
 public class AppController {
 	@Autowired
 	private ProductService service;
+	
+	@RequestMapping("/")
+	public String viewHomePage(Model model) {
+		List<Product> listProducts = service.listAll();
+		return "index";
+	}
 }

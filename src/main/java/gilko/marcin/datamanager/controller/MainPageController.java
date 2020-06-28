99c7f61190de6b0e8f35cc303898d14e3fb34c8d
gilkom <gilek06@op.pl>
2020-06-28 21:29:31 +0200

    adding i18n to product list menu

--- src/main/java/gilko/marcin/datamanager/controller/MainPageController.java
+++ src/main/java/gilko/marcin/datamanager/controller/MainPageController.java
@@ -14,17 +14,6 @@ public class MainPageController {
 	@GetMapping("/")
 	public String viewMainPage(Model model, HttpServletRequest request) {
 		model.addAttribute("pageTitle", "Home");
-		
-		Locale currentLocale= request.getLocale();
-		String countryCode = currentLocale.getCountry();
-		String countryName = currentLocale.getDisplayCountry();
-		
-		String langCode = currentLocale.getLanguage();
-		String langName = currentLocale.getDisplayLanguage();
-		
-		System.out.println(countryCode + ": " + countryName);
-		System.out.println(langCode + ": " + langName);
-		
 		return "index";
 	}
 }
